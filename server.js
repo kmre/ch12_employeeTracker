daf9d289325aad8c649428646dcb4af160a88846
kmre <karla.romero7@gmail.com>
2021-08-17 16:16:21 -0400

    working on wireframe

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res) => {
 // Start server after DB connection
 db.connect(err => {
   if (err) throw err;
-  console.log('Database connected.');
+  console.log('Database connected!');
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
